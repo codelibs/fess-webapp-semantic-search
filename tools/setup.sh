d9ae959370928a6c9609c1da9b773bd2f975c12c
Shinsuke Sugaya <shinsuke@apache.org>
2023-07-01 16:13:01 +0900

    add model group

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,6 +4,10 @@ opensearch_host=$1
 pipeline_name=neural_pipeline
 tmp_file=/tmp/output.$$
 
+# https://opensearch.org/docs/latest/ml-commons-plugin/pretrained-models/
+model_name=huggingface/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2
+#model_name=huggingface/sentence-transformers/all-MiniLM-L12-v2
+
 if ! which curl > /dev/null; then
   echo "curl command is not found."
   exit 1
@@ -24,19 +28,27 @@ if ! curl -o ${tmp_file} -s -XGET -H "Content-Type:application/json" "${opensear
   exit 1
 fi
 
-echo "Uploading language model..."
-curl -o ${tmp_file} -s -XPOST -H "Content-Type:application/json" "${opensearch_host}/_plugins/_ml/models/_upload" \
+echo "Registering a language model..."
+curl -o ${tmp_file} -s -XPOST -H "Content-Type:application/json" "${opensearch_host}/_plugins/_ml/model_groups/_register" \
 --data-raw '{
-  "name": "multi-qa-MiniLM-L6-cos-v1",
+  "name": "'${model_name}'",
+  "description": "A language model for semantic search."
+}'
+
+model_group_id=$(cat ${tmp_file} | jq -r .model_group_id)
+if [[ ${model_group_id} = "null" ]] ; then
+  echo "Failed to run a task: "$(cat ${tmp_file})
+  rm -f $tmp_file
+  exit 1
+fi
+
+echo "Uploading language model..."
+curl -o ${tmp_file} -s -XPOST -H "Content-Type:application/json" "${opensearch_host}/_plugins/_ml/models/_register" \
+--data-raw '{
+  "name": "'${model_name}'",
   "version": "1.0.1",
-  "description": "language model",
   "model_format": "TORCH_SCRIPT",
-  "model_config": {
-    "model_type": "bert",
-    "embedding_dimension": 384,
-    "framework_type": "sentence_transformers"
-  },
-  "url": "https://artifacts.opensearch.org/models/ml-models/huggingface/sentence-transformers/multi-qa-MiniLM-L6-cos-v1/1.0.1/torch_script/sentence-transformers_multi-qa-MiniLM-L6-cos-v1-1.0.1-torch_script.zip"
+  "model_group_id": "'${model_group_id}'"
 }'
 
 task_id=$(cat ${tmp_file} | jq -r .task_id)
