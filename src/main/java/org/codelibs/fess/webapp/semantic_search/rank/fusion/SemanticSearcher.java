f8a0e5aa312ef7edeeedcb44f0ba0fa19a0e7a8d
Shinsuke Sugaya <shinsuke@apache.org>
2023-07-03 22:03:23 +0900

    add SearchRequestParamsWrapper

--- src/main/java/org/codelibs/fess/webapp/semantic_search/rank/fusion/SemanticSearcher.java
+++ src/main/java/org/codelibs/fess/webapp/semantic_search/rank/fusion/SemanticSearcher.java
@@ -15,10 +15,16 @@
  */
 package org.codelibs.fess.webapp.semantic_search.rank.fusion;
 
+import java.util.Locale;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.codelibs.fess.entity.FacetInfo;
+import org.codelibs.fess.entity.GeoInfo;
+import org.codelibs.fess.entity.HighlightInfo;
 import org.codelibs.fess.entity.SearchRequestParams;
 import org.codelibs.fess.mylasta.action.FessUserBean;
 import org.codelibs.fess.rank.fusion.DefaultSearcher;
@@ -44,8 +50,9 @@ public class SemanticSearcher extends DefaultSearcher {
     protected SearchResult search(final String query, final SearchRequestParams params, final OptionalThing<FessUserBean> userBean) {
         final SemanticSearchHelper semanticSearchHelper = getSemanticSearchHelper();
         try {
-            semanticSearchHelper.createContext(query, params, userBean);
-            return super.search(query, params, userBean);
+            final SearchRequestParams reqParams = new SearchRequestParamsWrapper(params);
+            semanticSearchHelper.createContext(query, reqParams, userBean);
+            return super.search(query, reqParams, userBean);
         } finally {
             semanticSearchHelper.closeContext();
         }
@@ -54,4 +61,93 @@ public class SemanticSearcher extends DefaultSearcher {
     protected SemanticSearchHelper getSemanticSearchHelper() {
         return ComponentUtil.getComponent(SemanticSearchConstants.SEMANTIC_SEARCH_HELPER);
     }
+
+    protected static class SearchRequestParamsWrapper extends SearchRequestParams {
+        private final SearchRequestParams parent;
+
+        protected SearchRequestParamsWrapper(final SearchRequestParams params) {
+            this.parent = params;
+        }
+
+        @Override
+        public String getQuery() {
+            return parent.getQuery();
+        }
+
+        @Override
+        public Map<String, String[]> getFields() {
+            return parent.getFields();
+        }
+
+        @Override
+        public Map<String, String[]> getConditions() {
+            return parent.getConditions();
+        }
+
+        @Override
+        public String[] getLanguages() {
+            return parent.getLanguages();
+        }
+
+        @Override
+        public GeoInfo getGeoInfo() {
+            return null;
+        }
+
+        @Override
+        public FacetInfo getFacetInfo() {
+            return null;
+        }
+
+        @Override
+        public HighlightInfo getHighlightInfo() {
+            return null;
+        }
+
+        @Override
+        public String getSort() {
+            return parent.getSort();
+        }
+
+        @Override
+        public int getStartPosition() {
+            return parent.getStartPosition();
+        }
+
+        @Override
+        public int getPageSize() {
+            return parent.getPageSize();
+        }
+
+        @Override
+        public int getOffset() {
+            return parent.getOffset();
+        }
+
+        @Override
+        public String[] getExtraQueries() {
+            return parent.getExtraQueries();
+        }
+
+        @Override
+        public Object getAttribute(final String name) {
+            return parent.getAttribute(name);
+        }
+
+        @Override
+        public Locale getLocale() {
+            return parent.getLocale();
+        }
+
+        @Override
+        public SearchRequestType getType() {
+            return parent.getType();
+        }
+
+        @Override
+        public String getSimilarDocHash() {
+            return parent.getSimilarDocHash();
+        }
+    }
+
 }
